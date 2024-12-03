791e9f1e8a6218198b5761d06124c4822695ec62
James Lewis <jamesblewislewis@gmail.com>
2024-12-04 09:50:10 +1100

    push aoc answers

--- 2024/3/p1.py
+++ 2024/3/p1.py
@@ -1,4 +1,24 @@
-with open("s_input") as input:
-    for line in input:
-        # nums = line.strip().split(" ")
+import re
 
+with open("input") as input:
+    input_string = ""
+    for line in input:
+        input_string += line.strip()
+    total = 0
+    # Find all words that end with 'ain'
+    matches = re.findall(r'mul\([0-9][0-9]?[0-9]?,[0-9][0-9]?[0-9]?\)', input_string)
+    #print(matches)
+    for match in matches:
+        n1 = 0
+        n2 = 0
+        for c in range(len(match)):
+            if match[c] == ',':
+                n1 = int(match[4:c])
+                n2 = int(match[c+1:-1])
+                working = True
+                break
+        total += (n1 * n2)
+    #print(total)
+
+
+# incorrect 31341460
--- 2024/3/p2.py
+++ 2024/3/p2.py
@@ -1,4 +1,36 @@
-with open("s_input") as input:
-    for line in input:
-        # nums = line.strip().split(" ")
+import re
 
+with open("input") as input:
+    input_string = ""
+    for line in input:
+        input_string += line.strip()
+    total = 0
+    # Find all words that end with 'ain'
+    matches = re.findall(r"(mul\([0-9][0-9]?[0-9]?,[0-9][0-9]?[0-9]?\))|(do\(\))|(don't\(\))", input_string)
+    print(matches)
+    on=True
+    for match in matches:
+        # determine match kind
+        if match[0] != '': # it's a mul
+            if not on: # skip
+                continue
+            n1 = 0
+            n2 = 0
+            for c in range(len(match[0])):
+                if match[0][c] == ',':
+                    n1 = int(match[0][4:c])
+                    n2 = int(match[0][c+1:-1])
+                    working = True
+                    break
+            total += (n1 * n2)
+        elif match[1] != '':
+            on=True
+        elif match[2] != '':
+            on=False
+        else:
+            print(match)
+            raise Exception("this should not happen")
+    print(total)
+
+
+# incorrect 8133565
